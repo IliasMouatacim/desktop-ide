a0569db15a10e5f8bc2a64785a0d34050ea964ed
IliasMouatacim <ilias.mouatacim@gmail.com>
2026-04-02 01:37:43 +0200

    EMERGENCY HOTFIX: Revert monaco-vscode-api architecture to fix blank screen crash

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import vsixPlugin from '@codingame/monaco-vscode-rollup-vsix-plugin';
 
 export default defineConfig({
-  plugins: [react(), vsixPlugin()],
-  optimizeDeps: {
-    include: ['vscode', '@codingame/monaco-vscode-api']
-  },
-  resolve: {
-    alias: {
-      vscode: '@codingame/monaco-vscode-api'
-    }
-  },
-  build: {
-    rollupOptions: {
-      external: ['@codingame/monaco-vscode-api/vscode/vs/base/browser/cssValue']
-    }
-  },
+  plugins: [react()],
   server: {
     port: 5173,
     proxy: {
